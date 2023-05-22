9f598b62cc35b6d819ec3a452bb528cfdc78ce54
Artashes Ter-Abrahamyan <60017912+ArtashesTerAbramian@users.noreply.github.com>
2023-05-22 18:24:40 +0400

    Admin model

---
+++ BookStore.DAL/Configuration/AdminConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.DAL.Configuration;
+
+public class AdminConfiguration : BaseConfiguration<Admin>
+{
+    public override void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("admin");
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(256);
+
+        builder.Property(x => x.UserName)
+            .IsRequired()
+            .HasMaxLength(512);
+
+        builder.Property(x => x.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(512);
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(256);
+
+        builder.HasIndex(x => x.UserName)
+            .IsUnique();
+
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+    }
+}
---
+++ BookStore.DAL/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.DAL.Models;
+
+public class Admin : BaseEntity
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string PasswordHash { get; set; }
+}
